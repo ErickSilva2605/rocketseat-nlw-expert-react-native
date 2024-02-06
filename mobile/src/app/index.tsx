80382492d6af820962429c0e98e482498cf4097b
ErickSilva2605 <erick.dev.it@gmail.com>
2024-02-06 02:12:42 -0300

    add nativewind configuration

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+export default function Home() {
+  return (
+    <View className="flex-1 pt-12">
+      <Text className="text-white text-xl">
+        Hello React Native
+      </Text>
+    </View>
+  )
+}
