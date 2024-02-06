c04b7e9cade7efb84fe2c6b1269b565a9d2ba096
ErickSilva2605 <erick.dev.it@gmail.com>
2024-02-06 02:37:01 -0300

    add fonts configuration and loading component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from 'react-native'
+import { Text, View } from "react-native"
 
 export default function Home() {
   return (
     <View className="flex-1 pt-12">
-      <Text className="text-white text-xl">
+      <Text className="text-white text-xl font-heading">
         Hello React Native
       </Text>
     </View>
