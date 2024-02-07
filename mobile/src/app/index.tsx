6e217248764be6cac8615b2dea22a791709a0e35
ErickSilva2605 <erick.dev.it@gmail.com>
2024-02-06 22:47:37 -0300

    add header component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,11 +1,10 @@
-import { Text, View } from "react-native"
+import { Header } from "@/components/header"
+import { View } from "react-native"
 
 export default function Home() {
   return (
     <View className="flex-1 pt-12">
-      <Text className="text-white text-xl font-heading">
-        Hello React Native
-      </Text>
+      <Header title="Faça seu pedido" cartQuantityItems={1}/>
     </View>
   )
 }
