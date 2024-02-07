17dedee06efdf0b0033259115dd0957f9e9641b7
ErickSilva2605 <erick.dev.it@gmail.com>
2024-02-07 00:40:55 -0300

    add product componente

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,14 +1,31 @@
-import { useState } from "react"
-import { View, FlatList } from "react-native"
+import { useState, useRef } from "react"
+import { View, FlatList, SectionList, Text } from "react-native"
+
 import { Header } from "@/components/header"
 import { CategoryButton } from "@/components/category-button"
-import { CATEGORIES } from '@/utils/data/products'
+import { Product } from "@/components/product"
+
+import { CATEGORIES, MENU } from '@/utils/data/products'
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0])
 
+  const sectionListRef = useRef<SectionList>(null)
+
   function handleCategorySelected(selectedCategory: string) {
     setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory
+    )
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0,
+      })
+    }
   }
 
   return (
@@ -26,9 +43,28 @@ export default function Home() {
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
-        className="max-h-10 mt-5"
+        className="max-h-10 mt-5 mb-3"
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
       />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Product data={item} />
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
+
     </View>
   )
 }
