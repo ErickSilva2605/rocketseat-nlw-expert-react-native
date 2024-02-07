6adb24045e34f00e3ebde9e43b9df19e1d406ad4
ErickSilva2605 <erick.dev.it@gmail.com>
2024-02-06 23:42:36 -0300

    add category button component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,10 +1,34 @@
+import { useState } from "react"
+import { View, FlatList } from "react-native"
 import { Header } from "@/components/header"
-import { View } from "react-native"
+import { CategoryButton } from "@/components/category-button"
+import { CATEGORIES } from '@/utils/data/products'
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelected(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
   return (
-    <View className="flex-1 pt-12">
-      <Header title="Faça seu pedido" cartQuantityItems={1}/>
+    <View className="flex-1 pt-14">
+      <Header title="Faça seu pedido" cartQuantityItems={1} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelected(item)} />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="max-h-10 mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   )
 }
